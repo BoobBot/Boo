c9cfd7afed32a12ff8c0c80e6a5c04b3ab5238a5
Dyna <goldenleaf12@gmail.com>
2023-10-02 22:46:13 +0300

    i touched the ghost

--- src/boo.py
+++ src/boo.py
@@ -14,15 +14,18 @@ def get_troll():
         return random.choice(json.load(f)["trickortroll"])
 
 
-async def haunted(bot, guild_id, interval=300):
+async def haunted(bot, guild_id, role_id, interval=300):
     while bot.loop.is_running():
         home = bot.get_guild(int(guild_id))
         if not home:
             continue
         channels = [c for c in home.channels if isinstance(c, discord.TextChannel)]
-        
-        target = [m for m in home.members if not m.bot]
-        target.extend([r for r in home.roles if r.mentionable and r != home.default_role])
+        role = await discord.utils.get(home.roles, id=role_id)
+        if role:
+            target = role.members
+        else:
+            target = [m for m in home.members if not m.bot]
+            target.extend([r for r in home.roles if r.mentionable and r != home.default_role])
 
         try:
             random.shuffle(channels)
@@ -30,18 +33,19 @@ async def haunted(bot, guild_id, interval=300):
             channel = random.SystemRandom().choice(channels)
             ping = random.SystemRandom().choice(target)
             troll = get_troll()
-            msg = await channel.send("{}, {}".format(ping.mention, troll))
+            await channel.send(f"{ping.mention}, {troll}", delete_after=5)
             print("({}) {}, {}".format(str(channel), str(ping), troll))
-            await msg.delete()
+
         except discord.HTTPException:
             continue
         await asyncio.sleep(interval)
 
 
 class HauntedGuild(discord.Client):
-    def __init__(self, guild_id, interval=300, **options):
+    def __init__(self, guild_id, role_id, interval=300, **options):
         super().__init__(**options)
         self.guild_id = guild_id
+        self.role_id = role_id
         self.interval = interval
 
     async def on_ready(self):
@@ -49,16 +53,17 @@ class HauntedGuild(discord.Client):
         if not self.get_guild(int(self.guild_id)):
             print("error no guild with id of {} found did you add the bot?".format(self.guild_id))
             exit(-1)
-        await haunted(self, self.guild_id, interval=self.interval)
+        await haunted(self, self.guild_id, self.role_id, interval=self.interval)
 
 
 @click.command()
 @click.option('--token', prompt="Bot Token?", help="Discord bot token")
-@click.option("--guild_id", prompt="Guild id", help="id of your discord server")
-@click.option("--interval", default=300, prompt="Spook interval",
+@click.option('--guild_id', prompt="Guild id", help="id of your discord server")
+@click.option('--role_id', prompt="Role id", help="id of the role")
+@click.option('--interval', default=300, prompt="Spook interval",
               help="Duration between spooky sayings")
-def start(token, guild_id, interval=300):
-    bot = HauntedGuild(guild_id, interval=interval, intents=discord.Intents().all())
+def start(token, guild_id, role_id, interval=300):
+    bot = HauntedGuild(guild_id, role_id, interval=interval, intents=discord.Intents().all())
     bot.run(token)
 
 
