e7f342c9d536352561585c784303700e3e77323a
Dyna <goldenleaf12@gmail.com>
2023-10-04 20:58:20 +0300

    category specific bc everyone hates fun

--- src/boo.py
+++ src/boo.py
@@ -14,13 +14,20 @@ def get_troll():
         return random.choice(json.load(f)["trickortroll"])
 
 
-async def haunted(bot, guild_id, role_id, interval=300):
+async def haunted(bot, guild_id, role_id, category_id, interval=300):
     while bot.loop.is_running():
         home = bot.get_guild(int(guild_id))
         if not home:
             continue
-        channels = [c for c in home.channels if isinstance(c, discord.TextChannel)]
+        category = discord.utils.get(home.categories, id=int(category_id))
         role = discord.utils.get(home.roles, id=int(role_id))
+        if category:
+            channels = category.channels
+            print(f"found category using {category.name}")
+        else:
+            channels = home.text_channels
+            print("no category found using all text channels")
+
         if role:
             target = role.members
             print(f"role found using {role.name}")
@@ -44,10 +51,11 @@ async def haunted(bot, guild_id, role_id, interval=300):
 
 
 class HauntedGuild(discord.Client):
-    def __init__(self, guild_id, role_id, interval=300, **options):
+    def __init__(self, guild_id, role_id, category_id, interval=300, **options):
         super().__init__(**options)
         self.guild_id = guild_id
         self.role_id = role_id
+        self.category_id = category_id
         self.interval = interval
 
     async def on_ready(self):
@@ -55,17 +63,18 @@ class HauntedGuild(discord.Client):
         if not self.get_guild(int(self.guild_id)):
             print("error no guild with id of {} found did you add the bot?".format(self.guild_id))
             exit(-1)
-        await haunted(self, self.guild_id, self.role_id, interval=self.interval)
+        await haunted(self, self.guild_id, self.role_id, self.category_id, interval=self.interval)
 
 
 @click.command()
 @click.option('--token', prompt="Bot Token?", help="Discord bot token")
 @click.option('--guild_id', prompt="Guild id", help="id of your discord server")
 @click.option('--role_id', prompt="Role id", help="id of the role")
+@click.option('--category_id', prompt="Category id", help="id of category")
 @click.option('--interval', default=300, prompt="Spook interval",
               help="Duration between spooky sayings")
-def start(token, guild_id, role_id, interval=300):
-    bot = HauntedGuild(guild_id, role_id, interval=interval, intents=discord.Intents().all())
+def start(token, guild_id, role_id, category_id, interval=300):
+    bot = HauntedGuild(guild_id, role_id, category_id, interval=interval, intents=discord.Intents().all())
     bot.run(token)
 
 
